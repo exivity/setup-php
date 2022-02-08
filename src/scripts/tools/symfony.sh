b44a3cc07ca491c1e03a9c4abece26c56e500086
Shivam Mathur <shivam_jpr@hotmail.com>
2022-02-09 03:33:41 +0530

    Release v2-verbose

--- src/scripts/tools/symfony.sh
+++ src/scripts/tools/symfony.sh
@@ -1,11 +1,11 @@
 add_symfony() {
   if [ "$(uname -s)" = "Linux" ]; then
-    echo 'deb [trusted=yes] https://repo.symfony.com/apt/ /' | sudo tee /etc/apt/sources.list.d/symfony-cli.list >/dev/null 2>&1
+    echo 'deb [trusted=yes] https://repo.symfony.com/apt/ /' | sudo tee /etc/apt/sources.list.d/symfony-cli.list 
     update_lists symfony repo.symfony.com
     install_packages symfony-cli
   elif [ "$(uname -s)" = "Darwin" ]; then
     add_brew_tap symfony-cli/homebrew-tap
-    brew install symfony-cli/tap/symfony-cli >/dev/null 2>&1
+    brew install symfony-cli/tap/symfony-cli 
   fi
   sudo ln -s "$(command -v symfony)" "${tool_path_dir:?}"/symfony-cli
   tool_version=$(get_tool_version "symfony" "-V")
